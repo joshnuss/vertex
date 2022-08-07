97be669b8b119af2110a1b249e3ad95ce590b4dc
Josh Nussbaum <joshnuss@gmail.com>
2022-08-07 00:55:40 -0400

    Adds clickhouse backend

---
+++ lib/analytics/backend/clickhouse.ex
@@ -0,0 +1,30 @@
+defmodule Analytics.Backend.Clickhouse do
+  @behaviour Analytics.Backend
+
+  @config Application.get_env(:analytics, :clickhouse)
+  @database Keyword.fetch!(@config, :database)
+  @url Keyword.fetch!(@config, :url)
+  @req Req.new(base_url: @url)
+
+  def record(metric) do
+    query = build_query(metric)
+
+    Req.post!(@req,
+      url: "/database=#{@database}&query=",
+      body: query)
+  end
+
+  defp build_query(metric) do
+    tags = Enum.map_join(metric.tags, ",", & "'#{&1}'")
+
+    """
+    INSERT INTO metrics (project, account_id, event, tags)
+    VALUES (
+      '#{metric.project}',
+      '#{metric.account_id}',
+      '#{metric.event}',
+      [#{tags}]
+    )
+    """
+  end
+end
---
+++ test/analytics/backends/clickhouse_test.exs
@@ -0,0 +1,66 @@
+defmodule Analytics.Backend.ClickhouseTest do
+  use ExUnit.Case
+
+  alias Plug.Conn
+  alias Analytics.Metric
+  alias Analytics.Backend.Clickhouse
+
+  setup do
+    bypass = Bypass.open(port: 6001)
+
+    %{ bypass: bypass }
+  end
+
+  test "sends a metric to database", %{ bypass: bypass } do
+    Bypass.expect_once(bypass, "POST", "/database=analytics&query=", fn conn ->
+      {:ok, body, conn} = Conn.read_body(conn)
+
+      assert body == """
+      INSERT INTO metrics (project, account_id, event, tags)
+      VALUES (
+        'example',
+        '123',
+        'access.login.success',
+        []
+      )
+      """
+
+      Conn.resp(conn, 200, "")
+    end)
+
+    metric = %Metric{
+      project: "example",
+      account_id: "123",
+      event: "access.login.success"
+    }
+
+    Clickhouse.record(metric)
+  end
+
+  test "sends a metric to database with tags", %{ bypass: bypass } do
+    Bypass.expect_once(bypass, "POST", "/database=analytics&query=", fn conn ->
+      {:ok, body, conn} = Conn.read_body(conn)
+
+      assert body == """
+      INSERT INTO metrics (project, account_id, event, tags)
+      VALUES (
+        'example',
+        '123',
+        'access.login.success',
+        ['test','staging']
+      )
+      """
+
+      Conn.resp(conn, 200, "")
+    end)
+
+    metric = %Metric{
+      project: "example",
+      account_id: "123",
+      event: "access.login.success",
+      tags: ["test", "staging"]
+    }
+
+    Clickhouse.record(metric)
+  end
+end
